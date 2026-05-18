46cf435cdc8524df75d7e3ae16e285f98df7a4d3
NeuralKaizen <neuralkaizen@gmail.com>
2026-05-18 11:54:01 -0500

    avances 2

--- src/uniandes/dpoo/swing/interfaz/mapa/VentanaMapa.java
+++ src/uniandes/dpoo/swing/interfaz/mapa/VentanaMapa.java
@@ -1,6 +1,7 @@
 package uniandes.dpoo.swing.interfaz.mapa;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -49,14 +50,35 @@ public class VentanaMapa extends JFrame implements ActionListener
     public VentanaMapa( VentanaPrincipal ventanaPrincipal, List<Restaurante> restaurantes )
     {
         this.ventanaPrincipal = ventanaPrincipal;
+        setLayout( new BorderLayout( ) );
 
         // Agrega el panel donde se muestra el mapa
-        // TODO completar
+        panelMapa = new PanelMapaVisualizar( );
+        panelMapa.actualizarMapa( restaurantes );
+        add( panelMapa, BorderLayout.CENTER );
 
         // Agrega el panel con los RadioButtons y los configura
-        // TODO completar
+        JPanel panelRadios = new JPanel( new FlowLayout( ) );
+
+        radioTodos = new JRadioButton( "Todos" );
+        radioTodos.setActionCommand( TODOS );
+        radioTodos.addActionListener( this );
+        radioTodos.setSelected( true );
+
+        radioVisitados = new JRadioButton( "Visitados" );
+        radioVisitados.setActionCommand( VISITADOS );
+        radioVisitados.addActionListener( this );
+
+        ButtonGroup grupo = new ButtonGroup( );
+        grupo.add( radioTodos );
+        grupo.add( radioVisitados );
+
+        panelRadios.add( radioTodos );
+        panelRadios.add( radioVisitados );
+        add( panelRadios, BorderLayout.SOUTH );
 
         // Termina de configurar la ventana y la muestra
+        setTitle( "Mapa de Restaurantes" );
         pack( );
         setResizable( false );
         setDefaultCloseOperation( DISPOSE_ON_CLOSE );
